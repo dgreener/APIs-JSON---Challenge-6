36a8c9480440f19d600617efe68f79d06998825a
dgreener <131298133+dgreener@users.noreply.github.com>
2023-11-17 02:03:08 -0500

    Add files via upload

---
+++ api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "d1798748c51b63ad1a90f5d75b49c510"
+
+# Geoapify API Key
+geoapify_key = "0a248631e1be41e99f29bbc582ac3eb7"
---
+++ config.py
@@ -0,0 +1,5 @@
+# Weather API Key
+weather_api_key = "d1798748c51b63ad1a90f5d75b49c510"
+
+#Geoapify API Key
+geo_api_key = "0a248631e1be41e99f29bbc582ac3eb7"
